d90d2ed8b4cdb1c6f11e5c2b76e91959522ac613
Olga Parfenova <oaparf@yandex.ru>
2023-11-05 12:49:28 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://96262c1c-3c6d-4775-8829-dcae04b36e1c.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
